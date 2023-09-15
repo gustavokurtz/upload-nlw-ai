5a53250725ca25f57042bfece8d19c706bdbb2f0
kurtzfinalboss <kurtzff32@gmail.com>
2023-09-15 02:13:09 -0300

    prompt lição vídeo

--- upload-ai-server/prisma/seed.ts
+++ upload-ai-server/prisma/seed.ts
@@ -55,7 +55,36 @@ Transcrição:
 '''`.trim()
     }
   })
+
+  await prisma.prompt.create({
+    data: {
+      title: 'Título Yube',
+      template: `Seu papel é gerar três títulos para um vídeo do YouTube.
+
+Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar os títulos.
+Abaixo você também receberá uma lista de títulos, use essa lista como referência para os títulos a serem gerados.
+
+Os títulos devem ter no máximo 60 caracteres.
+Os títulos devem ser chamativos e atrativos para maximizar os cliques.
+
+Retorne APENAS os três títulos em formato de lista como no exemplo abaixo:
+'''
+- Título 1
+- Título 2
+- Título 3
+'''
+
+Transcrição:
+'''
+{transcription}
+'''`.trim()
+    }
+  })
+
 }
+
+
+
 
 main()
   .then(async () => {
